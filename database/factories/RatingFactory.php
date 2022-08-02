6b9b4b322169752ab92e8f724dee76430b4b1b64
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2022-08-03 06:56:16 +0800

    add model and factory

---
+++ database/factories/RatingFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Book;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Rating>
+ */
+class RatingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'value' => fake()->randomDigitNotNull(),
+            'book_id' => Book::inRandomOrder()->first()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Book;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Rating;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,8 +18,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Author::factory(10)->create();
+        Book::factory(100)->create();
+        Category::factory(30)->create();
+        Rating::factory(500)->create();
 
+        // \App\Models\User::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
